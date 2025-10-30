2966efa89e86fca9a72d66c74abb6a2c6d6ac718
Juan David Molina H. <jdavidmolina@unipacifico.edu.co>
2025-10-30 00:35:57 -0500

    Delete app directory
    cambio

--- app/src/main/java/servicios/Notificable.java
+++
@@ -1,9 +0,0 @@
-package servicios;
-
-/**
- * Interfaz para objetos que pueden recibir notificaciones.
- * Polimorfismo: cualquier clase que implemente Notificable puede recibir mensajes.
- */
-public interface Notificable {
-    void enviarNotificacion(String mensaje);
-}
